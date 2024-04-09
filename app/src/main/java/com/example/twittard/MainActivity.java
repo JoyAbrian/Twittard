1346647e60b5ca1b27cf6eeacaa9c6edd55f4b9c
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-04-09 20:10:52 +0800

    applying footer items to Activity

--- app/src/main/java/com/example/twittard/MainActivity.java
+++ app/src/main/java/com/example/twittard/MainActivity.java
@@ -3,14 +3,20 @@ package com.example.twittard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView footerHome, footerSearch, footerCommunity, footerNotification, footerMail; // Footer Items
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        footerHome = findViewById(R.id.footerHome);
+        footerSearch = findViewById(R.id.footerSearch);
+        footerCommunity = findViewById(R.id.footerCommunity);
+        footerNotification = findViewById(R.id.footerNotification);
+        footerMail = findViewById(R.id.footerMail);
     }
 }
