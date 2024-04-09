889f502c32f55d5f783a69615dfeb130124f5ca0
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-04-09 20:36:42 +0800

    apply fragments and footer items active

--- app/src/main/java/com/example/twittard/MainActivity.java
+++ app/src/main/java/com/example/twittard/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.twittard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -19,5 +20,61 @@ public class MainActivity extends AppCompatActivity {
         footerCommunity = findViewById(R.id.footerCommunity);
         footerNotification = findViewById(R.id.footerNotification);
         footerMail = findViewById(R.id.footerMail);
+
+        homeFragment();
+
+        footerHome.setOnClickListener(v -> { homeFragment(); });
+        footerSearch.setOnClickListener(v -> { searchFragment(); });
+        footerCommunity.setOnClickListener(v -> { communityFragment(); });
+        footerNotification.setOnClickListener(v -> { notificationFragment(); });
+        footerMail.setOnClickListener(v -> { mailFragment(); });
+    }
+
+    private void inactiveFooter() {
+        footerHome.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.footer_home));
+        footerSearch.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.footer_search));
+        footerCommunity.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.footer_community));
+        footerNotification.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.footer_notification));
+        footerMail.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.footer_mail));
+    }
+
+    private void homeFragment() {
+        inactiveFooter();
+        footerHome.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.footer_home_active));
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer, new HomeFragment())
+                .commit();
+    }
+
+    private void searchFragment() {
+        inactiveFooter();
+        footerSearch.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.footer_search_active));
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer, new SearchFragment())
+                .commit();
+    }
+
+    private void communityFragment() {
+        inactiveFooter();
+        footerCommunity.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.footer_community_active));
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer, new CommunityFragment())
+                .commit();
+    }
+
+    private void notificationFragment() {
+        inactiveFooter();
+        footerNotification.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.footer_notification_active));
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer, new NotificationFragment())
+                .commit();
+    }
+
+    private void mailFragment() {
+        inactiveFooter();
+        footerMail.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.footer_mail_active));
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer, new SearchFragment())
+                .commit();
     }
 }
