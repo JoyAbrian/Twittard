748a0a9fd65ce908d78b0c7abb44e4d519611c3d
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-04-10 21:27:57 +0800

    search view

--- app/src/main/java/com/example/twittard/SearchFragment.java
+++ app/src/main/java/com/example/twittard/SearchFragment.java
@@ -7,11 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class SearchFragment extends Fragment {
     ArrayList<Search> searches;
+
     public SearchFragment() {
         // Required empty public constructor
     }
@@ -24,7 +27,24 @@ public class SearchFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        LinearLayout parents = rootView.findViewById(R.id.parents);
+
+        int searches_size = searches.size();
+        for (int i = 0; i < searches_size; i++) {
+            View searchView = LayoutInflater.from(getContext()).inflate(R.layout.template_search, null);
+            TextView searchType = searchView.findViewById(R.id.searchType);
+            TextView searchName = searchView.findViewById(R.id.searchName);
+            TextView searchPosts = searchView.findViewById(R.id.searchPosts);
+
+            searchType.setText(searches.get(i).getType());
+            searchName.setText(searches.get(i).getName());
+            searchPosts.setText(searches.get(i).getPosts());
+
+            parents.addView(searchView);
+        }
+
+        return rootView;
     }
 
     private static ArrayList<Search> generateDummySearches() {
