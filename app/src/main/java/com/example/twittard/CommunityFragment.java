7818565639774316406e8b6202f8468b01d33f9b
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-04-10 22:38:02 +0800

    community view

--- app/src/main/java/com/example/twittard/CommunityFragment.java
+++ app/src/main/java/com/example/twittard/CommunityFragment.java
@@ -5,18 +5,46 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class CommunityFragment extends Fragment {
+    ArrayList<Community> communities;
 
     public CommunityFragment() {
         // Required empty public constructor
     }
 
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        communities = generateDummyCommunities();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_community, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        LinearLayout parents = rootView.findViewById(R.id.parents);
+
+        int communities_size = communities.size();
+        for (int i = 0; i < communities_size; i++) {
+            View communityView = LayoutInflater.from(getContext()).inflate(R.layout.template_community, null);
+            ImageView communityPicture = communityView.findViewById(R.id.communityPicture);
+            TextView communityName = communityView.findViewById(R.id.communityName);
+            TextView communityMembers = communityView.findViewById(R.id.communityMembers);
+            TextView communityType = communityView.findViewById(R.id.communityType);
+
+            communityPicture.setImageResource(communities.get(i).getImage());
+            communityName.setText(communities.get(i).getName());
+            communityMembers.setText(communities.get(i).getMembers());
+            communityType.setText(communities.get(i).getType());
+
+            parents.addView(communityView);
+        }
+
+            return rootView;
     }
 
     private ArrayList<Community> generateDummyCommunities() {
