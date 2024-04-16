26f7a8f613576a4e307979dc64e57797da92d2d5
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-04-16 10:14:04 +0800

    adding tweet list in profile

--- app/src/main/java/com/example/twittard/ProfileActivity.java
+++ app/src/main/java/com/example/twittard/ProfileActivity.java
@@ -1,7 +1,10 @@
 package com.example.twittard;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -9,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -26,7 +31,6 @@ public class ProfileActivity extends AppCompatActivity {
     private TextView profileDate;
     private TextView profileFollowing;
     private TextView profileFollower;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,5 +60,64 @@ public class ProfileActivity extends AppCompatActivity {
         profileDate.setText(account.getSignDate());
         profileFollowing.setText(account.getFollowing());
         profileFollower.setText(account.getFollowers());
+
+        LinearLayout tweetList = findViewById(R.id.tweetList);
+
+        ArrayList<Tweet> accountTweets = new ArrayList<>();
+        for (Tweet tweet : DataSource.tweets) {
+            if (tweet.getAccount().getUsername().equals(account.getUsername())) {
+                accountTweets.add(tweet);
+            }
+        }
+
+        int tweetSize = accountTweets.size();
+        for (int i = 0; i < tweetSize; i++) {
+            View tweetView = LayoutInflater.from(this).inflate(R.layout.template_tweet, null);
+
+            CircleImageView accountPicture = tweetView.findViewById(R.id.accountPicture);
+            TextView accountFullname = tweetView.findViewById(R.id.accountFullname);
+            TextView accountUsername = tweetView.findViewById(R.id.accountUsername);
+            TextView tweetDate = tweetView.findViewById(R.id.tweetDate);
+            TextView tweetText = tweetView.findViewById(R.id.tweetText);
+            ImageView tweetDrawablePicture = tweetView.findViewById(R.id.tweetDrawablePicture);
+            ImageView tweetUriImage = tweetView.findViewById(R.id.tweetUriImage);
+            TextView tweetComments = tweetView.findViewById(R.id.tweetComments);
+            TextView tweetReposts = tweetView.findViewById(R.id.tweetReposts);
+            TextView tweetLikes = tweetView.findViewById(R.id.tweetLikes);
+            TextView tweetViews = tweetView.findViewById(R.id.tweetViews);
+
+            accountPicture.setImageResource(accountTweets.get(i).getAccount().getProfilePhoto());
+            accountFullname.setText(accountTweets.get(i).getAccount().getFullname());
+            accountUsername.setText(accountTweets.get(i).getAccount().getUsername());
+            tweetDate.setText(accountTweets.get(i).getDate());
+            tweetText.setText(accountTweets.get(i).getTweet());
+
+            if (accountTweets.get(i).getTweet() == null) {
+                tweetText.setVisibility(View.GONE);
+            } else {
+                tweetText.setVisibility(View.VISIBLE);
+                tweetText.setText(accountTweets.get(i).getTweet());
+            }
+
+            if (accountTweets.get(i).getImage() == 1 && accountTweets.get(i).getUriImage() == null) {
+                tweetUriImage.setVisibility(View.GONE);
+                tweetDrawablePicture.setVisibility(View.GONE);
+            } else if (accountTweets.get(i).getImage() != 1 && accountTweets.get(i).getUriImage() == null) {
+                tweetDrawablePicture.setVisibility(View.VISIBLE);
+                tweetUriImage.setVisibility(View.GONE);
+                tweetDrawablePicture.setImageResource(accountTweets.get(i).getImage());
+            } else if (accountTweets.get(i).getImage() == 1 && accountTweets.get(i).getUriImage() != null) {
+                tweetDrawablePicture.setVisibility(View.GONE);
+                tweetUriImage.setVisibility(View.VISIBLE);
+                tweetUriImage.setImageURI(accountTweets.get(i).getUriImage());
+            }
+
+            tweetComments.setText(accountTweets.get(i).getComment());
+            tweetReposts.setText(accountTweets.get(i).getRepost());
+            tweetLikes.setText(accountTweets.get(i).getLike());
+            tweetViews.setText(accountTweets.get(i).getView());
+
+            tweetList.addView(tweetView);
+        }
     }
 }
