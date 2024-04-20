7b7eb3d3e1f69ef3cad8a285aee74274f4899fe5
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-04-20 09:21:19 +0800

    fix community fragment

--- app/src/main/java/com/example/twittard/CommunityFragment.java
+++ app/src/main/java/com/example/twittard/CommunityFragment.java
@@ -26,7 +26,7 @@ public class CommunityFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_community, container, false);
         LinearLayout parents = rootView.findViewById(R.id.parents);
 
         int communities_size = communities.size();
@@ -46,20 +46,5 @@ public class CommunityFragment extends Fragment {
         }
 
             return rootView;
-    }
-
-    private ArrayList<Community> generateDummyCommunities() {
-        ArrayList<Community> communities = new ArrayList<>();
-        communities.add(new Community(R.drawable.communitydesignsphere, "The Design Sphere", "244rb", "Desain"));
-        communities.add(new Community(R.drawable.communityxfeedback, "X Communities Feedback", "33rb", ""));
-        communities.add(new Community(R.drawable.communityapple, "Apple", "148rb", "Teknologi"));
-        communities.add(new Community(R.drawable.communityartistonthechain, "Artists On The Chain", "111rb", "Seni"));
-        communities.add(new Community(R.drawable.communitysoftwareengineering, "Software Engineering", "25rb", "Perangkat Lunak"));
-        communities.add(new Community(R.drawable.communitymemes, "Memes", "131rb", "Seru-Seruan"));
-        communities.add(new Community(R.drawable.communityyankeestwitter, "Yankees Twitter", "14rb", "Bisbol"));
-        communities.add(new Community(R.drawable.communitydiabloiv, "Diablo IV", "17rb", "Game"));
-        communities.add(new Community(R.drawable.communitydogs, "Dogs", "94rb", "Hewan"));
-        communities.add(new Community(R.drawable.communitydc, "DC", "21rb", "Komik"));
-        return communities;
     }
 }
